66abdcc7da4f4ed482dd8e5897d522e22c3c0de0
11uis <lluisarco@yahoo.es>
2020-05-21 19:51:54 +0200

    stock-item component generated

--- src/app/components/stock-item/stock-item.component.ts
+++ src/app/components/stock-item/stock-item.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-item.component.css']
 })
 export class StockItemComponent implements OnInit {
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+  public  positiveChange: boolean;
+  public favorite: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.name = 'Stock-Item';
+    this.code = 'SI01';
+    this.price = 90;
+    this.previousPrice = 75;
+    this.positiveChange = this.price >= this.previousPrice;
+    this.favorite = false;
+  }
+  // Toggle the favorite property
+  toggleFavorite() {
+    console.log('We are toggling the favorite for this stock');
+    this.favorite = !this.favorite;
   }
 
 }
