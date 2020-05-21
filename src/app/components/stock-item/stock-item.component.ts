7e84676490a9ff507b4f83b745970cf8fe9c1dd1
11uis <lluisarco@yahoo.es>
2020-05-21 20:22:22 +0200

    Chapter 2 finished

--- src/app/components/stock-item/stock-item.component.ts
+++ src/app/components/stock-item/stock-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../../entities/stock';
 
 @Component({
   selector: 'app-stock-item',
@@ -6,27 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-item.component.css']
 })
 export class StockItemComponent implements OnInit {
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public  positiveChange: boolean;
-  public favorite: boolean;
+  public stock: Stock;
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.name = 'Stock-Item';
-    this.code = 'SI01';
-    this.price = 90;
-    this.previousPrice = 75;
-    this.positiveChange = this.price >= this.previousPrice;
-    this.favorite = false;
+  ngOnInit() {
+    this.stock = new Stock('Stock-Item', 'ST01', 90, 75);
   }
   // Toggle the favorite property
-  toggleFavorite() {
-    console.log('We are toggling the favorite for this stock');
-    this.favorite = !this.favorite;
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite for this stock', event);
+    this.stock.favorite = !this.stock.favorite;
   }
 
 }
